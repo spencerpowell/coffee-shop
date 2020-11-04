05a0aee824202a4eddeb2322c7c9726401961da2
spencerpowell <spencer@expapp.com>
2020-11-04 14:54:57 -0500

    Remove auto generated files from the rb template

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,27 +7,17 @@
  */
 
 import * as React from 'react';
-import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import { GlobalStyle } from '../styles/global-styles';
+import { GlobalStyle } from 'styles/global-styles';
 
-import { HomePage } from './containers/HomePage/Loadable';
-import { NotFoundPage } from './containers/NotFoundPage/Loadable';
+import { CoffeeShop } from './containers/CoffeeShop';
 
 export function App() {
   return (
     <BrowserRouter>
-      <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
-      >
-        <meta name="description" content="A React Boilerplate application" />
-      </Helmet>
-
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={HomePage} />
-        <Route component={NotFoundPage} />
+        <Route exact path="/" component={CoffeeShop} />
       </Switch>
       <GlobalStyle />
     </BrowserRouter>
